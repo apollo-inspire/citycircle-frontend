4a37d321422dabde44d2161df95e8a9bf957f8c6
LucasvdVegt <lpsvandervegt@gmail.com>
2024-05-16 16:17:31 +0200

    udpa

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
+import { Slot, Stack } from 'expo-router';
 
-export default function App() {
+const RootLayout = () => {
+  // return (
+  //   <>
+  //     <Text>Header</Text>
+  //     <Slot />
+  //     <Text>Footer</Text>
+  //   </>
+  // )
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Stack>
+      <Stack.Screen name="index" options={{headerShown: false}} />
+    </Stack>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default RootLayout

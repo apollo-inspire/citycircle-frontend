93baf4771199dca54d7bb6bd00e28b76a4224909
LucasvdVegt <lpsvandervegt@gmail.com>
2024-05-24 17:40:35 +0200

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#042f2e',
+        secondary: {
+          DEFAULT: "#f0fdfa",
+          100: "#ccfbf1",
+          200: "#99f6e4"
+        },
+        black: {
+          DEFAULT: "#000",
+          100: "#030712",
+          200: "#111827"
+        }
+        gray: {
+          100: "#4b5563"
+        }
+      }
+    },
   },
   plugins: [],
 }

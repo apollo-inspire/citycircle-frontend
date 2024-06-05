73ae7973b6bf0b3fac7cc0dad95c9c5f32d9baa7
LucasvdVegt <lpsvandervegt@gmail.com>
2024-06-05 17:58:13 +0200

    update

--- app/(tabs)/list.jsx
+++ app/(tabs)/list.jsx
@@ -1,11 +1,25 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList, Image } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import SearchInput from '../../components/SearchInput'
 
 const List = () => {
   return (
-    <View>
-      <Text>List</Text>
-    </View>
+    <SafeAreaView className="bg-primary">
+      <FlatList
+        data={[{ id: 1 }, { id: 2 }, { id: 3 },]}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => (
+           <Text className="text-3xl text-white">{item.id}</Text>
+        )}
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4 space-y-6"></View>
+        )}
+      >
+      </FlatList>
+      {/* <Text>List</Text> */}
+      <SearchInput />
+    </SafeAreaView>
   )
 }
 
